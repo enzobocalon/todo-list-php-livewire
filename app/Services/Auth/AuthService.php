18f1b89c6379416c246c8b4e687c83afbc624fdb
unknown <enzo.bocalon@hotmail.com>
2026-03-16 13:11:14 -0300

    feat: start api

--- app/Services/Auth/AuthService.php
+++ app/Services/Auth/AuthService.php
@@ -3,12 +3,25 @@
 namespace App\Services\Auth;
 
 use App\Models\User;
+use Exception;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 
 class AuthService
 {
     public function __construct() {}
+
+    public function login(array $credentials) {
+        if (!Auth::attempt($credentials)) {
+            throw new Exception('Credenciais inválidas.');
+        }
+
+        $user = Auth::user();
+        $user->tokens()->delete();
+
+        return $user->createToken('api-token')->plainTextToken;
+    }
 
     public function signup(array $data): User{
         return User::create([
